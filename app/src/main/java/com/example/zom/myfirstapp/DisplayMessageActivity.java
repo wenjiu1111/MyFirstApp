8ddcb15040e2528f20a65bd92b974cbde4880326
zom <zhwb1126@qq.com>
2016-06-05 22:01:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zom/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,26 @@
+package com.example.zom.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(90);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.display_content);
+        layout.addView(textView);
+    }
+}
